436193a3052fc321db61a17197f3ffaba0f70151
Seanneskie <seannecanete32@gmail.com>
2024-11-26 11:59:16 +0800

    nextjs config

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ const nextConfig: NextConfig = {
   output: "export", // Export the project as a static site
   basePath: "/bluefeather", // Set the base path to the repository name
   assetPrefix: "/bluefeather", // Set the asset prefix for static assets
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/bluefeather",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
